007d9e2303552f093773f2459697102a1791dd5e
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2015-12-11 19:29:46 +0100

    Add liteaxolotol from yowsup

---
+++ store/sqlite/liteprekeystore.py
@@ -0,0 +1,54 @@
+from axolotl.state.prekeyrecord import PreKeyRecord
+from axolotl.state.prekeystore import PreKeyStore
+
+
+class LitePreKeyStore(PreKeyStore):
+    def __init__(self, dbConn):
+        """
+        :type dbConn: Connection
+        """
+        self.dbConn = dbConn
+        dbConn.execute("CREATE TABLE IF NOT EXISTS prekeys(" +
+                       "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                       "prekey_id INTEGER UNIQUE, sent_to_server BOOLEAN, " +
+                       " record BLOB);")
+
+    def loadPreKey(self, preKeyId):
+        q = "SELECT record FROM prekeys WHERE prekey_id = ?"
+
+        cursor = self.dbConn.cursor()
+        cursor.execute(q, (preKeyId, ))
+
+        result = cursor.fetchone()
+        if not result:
+            raise Exception("No such prekeyRecord!")
+
+        return PreKeyRecord(serialized=result[0])
+
+    def loadPendingPreKeys(self):
+        q = "SELECT record FROM prekeys"
+        cursor = self.dbConn.cursor()
+        cursor.execute(q)
+        result = cursor.fetchall()
+
+        return [PreKeyRecord(serialized=result[0]) for result in result]
+        # return [PreKeyRecord(serialized=result[0]) for r in result]
+
+    def storePreKey(self, preKeyId, preKeyRecord):
+        # self.removePreKey(preKeyId)
+        q = "INSERT INTO prekeys (prekey_id, record) VALUES(?,?)"
+        cursor = self.dbConn.cursor()
+        cursor.execute(q, (preKeyId, preKeyRecord.serialize()))
+        self.dbConn.commit()
+
+    def containsPreKey(self, preKeyId):
+        q = "SELECT record FROM prekeys WHERE prekey_id = ?"
+        cursor = self.dbConn.cursor()
+        cursor.execute(q, (preKeyId, ))
+        return cursor.fetchone() is not None
+
+    def removePreKey(self, preKeyId):
+        q = "DELETE FROM prekeys WHERE prekey_id = ?"
+        cursor = self.dbConn.cursor()
+        cursor.execute(q, (preKeyId, ))
+        self.dbConn.commit()
