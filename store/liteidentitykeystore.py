11f245935346caeafc1adca098dcf9972876fc93
Daniel Gultsch <daniel@gultsch.de>
2015-12-20 22:16:55 +0100

    prepare identity keystore to save trust state

--- store/liteidentitykeystore.py
+++ store/liteidentitykeystore.py
@@ -14,8 +14,7 @@ class LiteIdentityKeyStore(IdentityKeyStore):
             "CREATE TABLE IF NOT EXISTS identities (" +
             "_id INTEGER PRIMARY KEY AUTOINCREMENT," + "recipient_id TEXT," +
             "registration_id INTEGER, public_key BLOB, private_key BLOB," +
-            "next_prekey_id INTEGER, timestamp INTEGER, " +
-            "UNIQUE(recipient_id, registration_id));")
+            "next_prekey_id NUMBER, timestamp NUMBER, trust NUMBER);")
 
     def getIdentityKeyPair(self):
         q = "SELECT public_key, private_key FROM identities " + \
@@ -49,10 +48,6 @@ class LiteIdentityKeyStore(IdentityKeyStore):
         self.dbConn.commit()
 
     def saveIdentity(self, recipientId, identityKey):
-        q = "DELETE FROM identities WHERE recipient_id=?"
-        self.dbConn.cursor().execute(q, (recipientId, ))
-        self.dbConn.commit()
-
         q = "INSERT INTO identities (recipient_id, public_key) VALUES(?, ?)"
         c = self.dbConn.cursor()
         c.execute(q, (recipientId, identityKey.getPublicKey().serialize()))
@@ -60,10 +55,3 @@ class LiteIdentityKeyStore(IdentityKeyStore):
 
     def isTrustedIdentity(self, recipientId, identityKey):
         return True
-        q = "SELECT public_key from identities WHERE recipient_id = ?"
-        c = self.dbConn.cursor()
-        c.execute(q, (recipientId, ))
-        result = c.fetchone()
-        if not result:
-            return True
-        return result[0] == identityKey.getPublicKey().serialize()
